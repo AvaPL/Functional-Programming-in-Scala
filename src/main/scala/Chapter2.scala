31949a6ad10ec39eb9f7503598e742d606275022
AvaPL <241574@student.pwr.edu.pl>
2021-03-27 20:01:57 +0100

    Added missing implementations from chapter 2.

---
+++ src/main/scala/Chapter2.scala
@@ -0,0 +1,25 @@
+import scala.annotation.tailrec
+
+object Chapter2 {
+  def fib(n: Int): Int = {
+    @tailrec
+    def loop(n: Int, acc1: Int, acc2: Int): Int = {
+      if (n == 0) acc1
+      else if (n == 1) acc2
+      else loop(n - 1, acc2, acc1 + acc2)
+    }
+
+    loop(n, 0, 1)
+  }
+
+  def isSorted[A](as: Array[A], ordered: (A, A) => Boolean): Boolean =
+    as.sliding(2).forall {
+      case Array(x, y) => ordered(x, y)
+    }
+
+  def curry[A, B, C](f: (A, B) => C): A => (B => C) = a => (b => f(a, b))
+
+  def uncurry[A, B, C](f: A => B => C): (A, B) => C = (a, b) => f(a)(b)
+
+  def compose[A, B, C](f: B => C, g: A => B): A => C = a => f(g(a))
+}
---
+++ src/test/scala/Chapter2Test.scala
@@ -0,0 +1,38 @@
+import Chapter2._
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+class Chapter2Test extends AnyFlatSpec with Matchers {
+  "fib" should "return valid Fibonacci numbers" in {
+    fib(0) should be(0)
+    fib(1) should be(1)
+    fib(3) should be(2)
+    fib(6) should be(8)
+  }
+
+  "isSorted" should "return valid booleans" in {
+    isSorted(Array(1, 2, 3), (x: Int, y: Int) => x < y) should be(true)
+    isSorted(Array(5, 2, 0), (x: Int, y: Int) => x > y) should be(true)
+    isSorted(Array(1, 5, 2), (x: Int, y: Int) => x < y) should be(false)
+    isSorted(Array("Abc", "zeF", "Ghi"), (x: String, y: String) => x.toLowerCase.last < y.toLowerCase.last) should be(true)
+  }
+
+  "curry" should "curry function" in {
+    def foo(a: Int, b: Boolean) = s"$a$b"
+    val curriedFoo = curry(foo)
+    foo(5, b = true) should be(curriedFoo(5)(true))
+  }
+
+  "uncurry" should "uncurry function" in {
+    def foo(a: Int)(b: Boolean) = s"$a$b"
+    val uncurriedFoo = uncurry(foo)
+    foo(5)(b = true) should be(uncurriedFoo(5, true))
+  }
+
+  "compose" should "compose functions" in {
+    def foo(b: Boolean) = if(b) "Greater than 5" else "Less or equal 5"
+    def bar(a: Int) = a > 5
+    val composed = compose(foo, bar)
+    foo(bar(6)) should be(composed(6))
+  }
+}
