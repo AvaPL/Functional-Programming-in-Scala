8f7f8d2670d50acbf343cf6cd22f6c2e5c05a7ac
AvaPL <241574@student.pwr.edu.pl>
2021-04-25 22:32:41 +0200

    Finished implementing Option.

--- src/main/scala/chapter4/Option.scala
+++ src/main/scala/chapter4/Option.scala
@@ -1,5 +1,7 @@
 package chapter4
 
+import scala.collection.mutable.ListBuffer
+
 sealed trait Option[+A] {
 
   def map[B](f: A => B): Option[B] = this match {
@@ -20,6 +22,19 @@ sealed trait Option[+A] {
 
   def filter(f: A => Boolean): Option[A] =
     this.flatMap(value => if (f(value)) this else None)
+}
+
+object Option {
+
+  def map2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] =
+    a.flatMap(a => b.map(b => f(a, b)))
+
+  def sequence[A](list: List[Option[A]]): Option[List[A]] =
+    traverse(list)(identity)
+
+  // Against clean code but good for practicing functional folds :)
+  def traverse[A, B](list: List[A])(f: A => Option[B]): Option[List[B]] =
+    list.map(f).foldRight[Option[List[B]]](Some(Nil))(map2(_, _)(_ :: _))
 }
 
 case class Some[+A](value: A) extends Option[A]
--- src/test/scala/chapter4/OptionTest.scala
+++ src/test/scala/chapter4/OptionTest.scala
@@ -94,4 +94,72 @@ class OptionTest extends AnyWordSpec with Matchers {
       }
     }
   }
+
+  "map2" when {
+    "given two None" should {
+      "return None" in {
+        Option.map2(None, None)((_, _) => 5) should be(None)
+      }
+    }
+
+    "given one None" should {
+      "return None for left None" in {
+        Option.map2(None, Some(5))((_, _) => 5) should be(None)
+      }
+
+      "return None for right None" in {
+        Option.map2(Some(5), None)((_, _) => 5) should be(None)
+      }
+    }
+
+    "given two Some" should {
+      "return mapped value" in {
+        Option.map2(Some(5), Some(4.2))((a, b) => (a + b).toString) should be(Some("9.2"))
+      }
+    }
+  }
+
+  "sequence" when {
+    "given Nil" should {
+      "return Some(Nil)" in {
+        Option.sequence(Nil) should be(Some(Nil))
+      }
+    }
+
+    "given list with one element" should {
+      "return None for List(None)" in {
+        Option.sequence(List(None)) should be(None)
+      }
+
+      "return Some for List(Some)" in {
+        Option.sequence(List(Some(5))) should be(Some(List(5)))
+      }
+    }
+
+    "given list with multiple elements" should {
+      "return None for list of Nones" in {
+        Option.sequence(List(None, None, None)) should be(None)
+      }
+
+      "return None for list with one None" in {
+        Option.sequence(List(Some(5), None, Some(10))) should be(None)
+      }
+
+      "return Some for list with Somes" in {
+        Option.sequence(List(Some(1), Some(2), Some(3))) should be(Some(List(1, 2, 3)))
+      }
+    }
+  }
+
+  "traverse" when {
+    "given a list of values" should {
+      "return Some when function returns Some for all elements" in {
+        Option.traverse(List(1, 2, 3))(i => Some(i.toString)) should be(Some(List("1", "2", "3")))
+      }
+
+      "return None when function return None for at least one element" in {
+        Option.traverse(List(1, 2, 3))(i => if (i == 2) None else Some(i)) should be(None)
+      }
+    }
+  }
 }
