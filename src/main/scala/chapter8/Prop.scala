5bffcb0eb575689e62ccea7283c59694f8f4f52d
Paweł Cembaluk <241574@student.pwr.edu.pl>
2021-07-10 20:34:24 +0200

    Implemented forAll Prop method.

--- src/main/scala/chapter8/Prop.scala
+++ src/main/scala/chapter8/Prop.scala
@@ -1,12 +1,52 @@
 package chapter8
 
-import chapter8.Prop.{FailedCase, SuccessCount}
+import chapter6.Rng
+import chapter8.Prop.TestCases
 
-trait Prop {
-  def check: Either[(FailedCase, SuccessCount), SuccessCount]
-}
+import scala.annotation.tailrec
+import scala.util.{Failure, Success, Try}
+
+case class Prop(run: (TestCases, Rng) => Result)
 
 object Prop {
-  type FailedCase = String
-  type SuccessCount = Int
+  type TestCases = Int
+
+  def forAll[A](gen: Gen[A])(f: A => Boolean): Prop = {
+    Prop(ForAllPropChecker(gen, f, _).check(_))
+  }
+
+  private case class ForAllPropChecker[A](gen: Gen[A], f: A => Boolean, testCases: TestCases) {
+
+    def check(rng: Rng): Result =
+      check(casesLeft = testCases, rng)
+
+    @tailrec // Intentionally left long to keep it tailrec
+    private def check(casesLeft: Int, rng: Rng): Result = {
+      if (casesLeft <= 0)
+        Passed
+      else {
+        val (generatedValue, rng2) = gen.sample.run(rng)
+        val result = Try(f(generatedValue))
+        result match {
+          case Success(true) => check(casesLeft - 1, rng2)
+          case Success(false) => Falsified(failureMessage(generatedValue), successes = testCases - casesLeft)
+          case Failure(exception) => Falsified(failureMessage(generatedValue, exception), successes = testCases - casesLeft)
+        }
+      }
+    }
+
+    private def failureMessage(generatedValue: A): String =
+      s"Property was falsified for value: $generatedValue"
+
+    private def failureMessage(generatedValue: A, exception: Throwable) =
+      s"""Check failed with exception for value: $generatedValue
+         |${exception.getStackTrace.mkString("\n")}
+         |""".stripMargin
+  }
 }
+
+sealed trait Result
+
+case object Passed extends Result
+
+case class Falsified(failure: String, successes: Int) extends Result
