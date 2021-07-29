84e47cb1837da63e08ca549be246591e45377fa8
Paweł Cembaluk <241574@student.pwr.edu.pl>
2021-07-29 19:48:47 +0200

    Added tests for listOfN.

--- src/main/scala/chapter9/Parser.scala
+++ src/main/scala/chapter9/Parser.scala
@@ -4,6 +4,8 @@ trait Parser[T] {
   def run(input: String): Either[ParseError, T]
 
   def or(other: Parser[T]): Parser[T] = ???
+
+  def listOfN(n: Int): Parser[List[T]] = ???
 }
 
 object Parser {
--- src/test/scala/chapter9/ParserTest.scala
+++ src/test/scala/chapter9/ParserTest.scala
@@ -94,4 +94,68 @@ class ParserTest extends AnyWordSpec with Matchers {
       }
     }
   }
+
+  "listOfN" should {
+    "parse a string" when {
+      "number of occurrences matches the given number" in {
+        val string = "test"
+        val parser = Parser.string(string)
+        val parser3 = parser.listOfN(3)
+        val string3 = string * 3
+
+        val result = parser3.run(string3)
+
+        result should be(Right(string3))
+      }
+
+      "parser is a compound parser" in {
+        val char1 = 'a'
+        val char2 = 'b'
+        val string6 = char1.toString * 3 + char2.toString * 2 + char1.toString
+        val parser1 = Parser.char(char1)
+        val parser2 = Parser.char(char2)
+        val parserOr = parser1.or(parser2)
+        val parser6 = parserOr.listOfN(6)
+
+        val result = parser6.run(string6)
+
+        result should be(Right(string6))
+      }
+    }
+
+    "return ParseError" when {
+      "number of occurrences is smaller than the given number" in {
+        val string = "test"
+        val parser = Parser.string(string)
+        val parser3 = parser.listOfN(3)
+        val string6 = string * 6
+
+        val result = parser3.run(string6)
+
+        result should be(Right(string6))
+      }
+
+      "number of occurrences is greater than the given number" in {
+        val string = "test"
+        val parser = Parser.string(string)
+        val parser3 = parser.listOfN(3)
+        val string2 = string * 2
+
+        val result = parser3.run(string2)
+
+        result should be(Right(string2))
+      }
+
+      "matched tokens are not consecutive" in {
+        val string = "test"
+        val parser = Parser.string(string)
+        val parser3 = parser.listOfN(3)
+        val stringNonConsecutive = string * 2 + "other" + string
+
+        val result = parser3.run(stringNonConsecutive)
+
+        result should be(Right(stringNonConsecutive))
+      }
+    }
+  }
 }
