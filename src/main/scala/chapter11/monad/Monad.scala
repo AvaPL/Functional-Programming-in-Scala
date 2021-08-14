d1a8069c47de6b26d1fea1e7e013406bac27e292
Paweł Cembaluk <241574@student.pwr.edu.pl>
2021-08-14 13:11:13 +0200

    Added Functor and Monad.

---
+++ src/main/scala/chapter11/monad/Monad.scala
@@ -0,0 +1,68 @@
+package chapter11.monad
+
+import chapter11.functor.Functor
+import chapter7.Nonblocking.Par
+import chapter8.Gen
+import chapter9.Parser
+
+trait Monad[F[_]] extends Functor[F] {
+  def unit[A](a: A): F[A]
+
+  def flatMap[A, B](fa: F[A])(f: A => F[B]): F[B]
+
+  override def map[A, B](fa: F[A])(f: A => B): F[B] =
+    flatMap(fa)(a => unit(f(a)))
+
+  def map2[A, B, C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C] =
+    flatMap(fa)(a => map(fb)(b => f(a, b)))
+}
+
+object Monad {
+  val gen: Monad[Gen] = new Monad[Gen] {
+    override def unit[A](a: A): Gen[A] =
+      Gen.unit(a)
+
+    override def flatMap[A, B](fa: Gen[A])(f: A => Gen[B]): Gen[B] =
+      fa.flatMap(f)
+  }
+
+  val par: Monad[Par] = new Monad[Par] {
+    override def unit[A](a: A): Par[A] =
+      Par.unit(a)
+
+    override def flatMap[A, B](fa: Par[A])(f: A => Par[B]): Par[B] =
+      Par.flatMap(fa)(f)
+  }
+
+  val parser: Monad[Parser] = new Monad[Parser] {
+    override def unit[A](a: A): Parser[A] =
+      Parser.succeed(a)
+
+    override def flatMap[A, B](fa: Parser[A])(f: A => Parser[B]): Parser[B] =
+      fa.flatMap(f)
+  }
+
+  val option: Monad[Option] = new Monad[Option] {
+    override def unit[A](a: A): Option[A] =
+      Some(a)
+
+    override def flatMap[A, B](fa: Option[A])(f: A => Option[B]): Option[B] =
+      fa.flatMap(f)
+  }
+
+  val lazyList: Monad[LazyList] = new Monad[LazyList] {
+    override def unit[A](a: A): LazyList[A] =
+      LazyList(a)
+
+    override def flatMap[A, B](fa: LazyList[A])(f: A => LazyList[B]): LazyList[B] =
+      fa.flatMap(f)
+  }
+
+  val list: Monad[List] = new Monad[List] {
+    override def unit[A](a: A): List[A] =
+      List(a)
+
+    override def flatMap[A, B](fa: List[A])(f: A => List[B]): List[B] =
+      fa.flatMap(f)
+  }
+}
